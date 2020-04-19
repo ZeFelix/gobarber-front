4f878026da694b4c8b7fdebbbb506b1ab2a0fc27
zefelix <jophev3@hotmail.com>
2020-04-19 18:47:51 -0300

    fix routes

---
+++ src/routes/Route.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect path="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect path="/dashboard" />;
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
