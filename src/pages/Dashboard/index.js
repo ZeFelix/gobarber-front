8b340f5f4bafa83e28ed695e4661ae2c0df30ed6
zefelix <jophev3@hotmail.com>
2020-04-23 15:46:08 -0300

    styles dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import * as S from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <S.Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>data tal</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <S.Time>
+          <strong>horario</strong>
+          <span>nome do cidadão</span>
+        </S.Time>
+        <S.Time>
+          <strong>horario</strong>
+          <span>nome do cidadão</span>
+        </S.Time>
+        <S.Time>
+          <strong>horario</strong>
+          <span>nome do cidadão</span>
+        </S.Time>
+        <S.Time>
+          <strong>horario</strong>
+          <span>nome do cidadão</span>
+        </S.Time>
+      </ul>
+    </S.Container>
+  );
 }
